385eeeaabc13fba7c3a5b6c3a6317263db9b6c0b
aditya khanna <adityakhanna2009@gmail.com>
2020-06-14 19:37:34 +0530

    updated week9 assignment

--- week9/week9_student/src/main/java/com/example/demo/studentcontroller.java
+++ week9/week9_student/src/main/java/com/example/demo/studentcontroller.java
@@ -1,4 +1,5 @@
 package com.example.demo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
@@ -10,7 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class studentcontroller {
-    studentdao stddao;
+    @Autowired
+	studentdao stddao;
 	@GetMapping("/getid")
 	public String getform()
 	{
